93e36d5910d4f6ce2a922f5a7a6453a6d32961c9
SandyTC <138057110+SandyTC@users.noreply.github.com>
2024-06-21 11:03:29 +0100

    Added Bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/SandyTC" target="_blank" rel="noreferrer">
-          Sandy Trebilcock
-        </a>
-        , is open-sourced on{" "}
-        <a
-          href="https://github.com/SandyTC/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-        , and is hosted on{" "}
-        <a
-          href="https://sandys-react-weather.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/SandyTC" target="_blank" rel="noreferrer">
+            Sandy Trebilcock
+          </a>
+          , is open-sourced on{" "}
+          <a
+            href="https://github.com/SandyTC/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          , and is hosted on{" "}
+          <a
+            href="https://sandys-react-weather.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
