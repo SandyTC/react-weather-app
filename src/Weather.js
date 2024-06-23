a54dec50d8bd8e4c724e30665cb484aee82df409
SandyTC <138057110+SandyTC@users.noreply.github.com>
2024-06-23 12:09:36 +0100

    Basic HTML for Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,43 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search for a city:"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>London</h1>
+      <ul>
+        <li>Sunday 11:00</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Sunny"
+          />
+          21°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 53%</li>
+            <li>Wind: 5 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
